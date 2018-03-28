e8a9b6a2f49b37a67f20377b0b4d82a041cff015
Anton Liubimov <ant.liubimov@gmail.com>
2018-03-28 16:40:36 +0500

    Start to write js-script

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,29 @@
 'use strict';
 
-var api = 'api.openweathermap.org/data/2.5.weather?';
+var api = 'https://api.openweathermap.org/data/2.5/weather?';
 var lat, lon;
 var tempUnit = 'C';
 
-function getLocation() {
+window.onload = function() {
+    console.log("Document is ready");
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(currPosition);
     } else {
-        alert("Geolocation is not supported by this browser");
+        console.log("Geolocation is not supported by this browser");
     }
-}
 
-function currPosition(position) {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-}
 
-function gerWeather(lat, lon) {
+    function currPosition(position) {
+        lat = 'lat=' + Math.round(position.coords.latitude * 10) / 10;
+        lon = 'lon=' + Math.round(position.coords.longitude * 10) / 10;
+        getWeather(lat, lon);
+    };
 
-}
+    function getWeather(lat, lon) {
+        var urlStr = api + lat + '&' + lon;
+        var xhttp = new XMLHttpRequest();
+        xhttp.open('GET', urlStr, true);
+        xhttp.send();
+        console.log(xhttp.responseText);
+    };
+};
